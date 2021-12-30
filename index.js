1e4b0df339be8b9209d65aed07bcdb5eff527be2
CarlosGoncalves89 <71924673+CarlosGoncalves89@users.noreply.github.com>
2021-12-30 20:28:55 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const path = require('path'); // path can obtain a local path on server.
 
 const app = express(); // init express.
 const port = process.env.PORT || 3000; // select a default port if .env is not present.
+
+
+app.use('/css', express.static(__dirname + '/assets/css')); // create a folder on server, and link with static import
+app.use('/js', express.static(__dirname + '/assets/js')); // create a folder on server, and link with static import
